7e1af966f57f433f742c69cf409ad3619bdbe005
Jordan Marr <jordanmarr@users.noreply.github.com>
2026-02-19 00:09:58 -0500

    Added STJ extensions

---
+++ src/Serde.FS.STJ/StjExtensions.fs
@@ -0,0 +1,34 @@
+﻿namespace Serde.FS.STJ
+
+open System.Text.Json
+open Serde.FS
+
+type StjOptions(jsonOptions: JsonSerializerOptions) =
+    interface ISerdeOptions
+    member _.JsonOptions = jsonOptions
+
+type StjBackend() =
+    interface ISerdeBackend with
+        member _.Serialize(value, options) =
+            let opts =
+                match options with
+                | Some (:? StjOptions as o) -> o.JsonOptions
+                | _ -> JsonSerializerOptions()
+            JsonSerializer.Serialize(value, opts)
+
+        member _.Deserialize(json, options) =
+            let opts =
+                match options with
+                | Some (:? StjOptions as o) -> o.JsonOptions
+                | _ -> JsonSerializerOptions()
+            JsonSerializer.Deserialize<'T>(json, opts)
+
+[<AutoOpen>]
+module StjSerdeExtensions =
+
+    type Serde with
+        static member Serialize(value: 'T, options: StjOptions) =
+            Serde.DefaultBackend.Serialize(value, Some options)
+
+        static member Deserialize<'T>(json: string, options: StjOptions) =
+            Serde.DefaultBackend.Deserialize<'T>(json, Some options)
