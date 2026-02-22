7039e6110868127aec11defda4f9b500c04eabd8
Jordan Marr <jordanmarr@users.noreply.github.com>
2026-02-22 11:45:39 -0500

    Added StjOptionsCache with defaultJsonOptions to prevent expensive options reconstruction.

--- src/Serde.FS.STJ/StjBackend.fs
+++ src/Serde.FS.STJ/StjBackend.fs
@@ -3,10 +3,19 @@ namespace Serde.FS.STJ
 open System.Text.Json
 open Serde.FS
 
+/// Cached JsonSerializerOptions instance used by the STJ backend.
+/// Creating new options per call is expensive and breaks metadata caching.
+/// This instance is initialized once and reused for all serialization.
+module internal StjOptionsCache =
+    let defaultJsonOptions =
+        let opts = JsonSerializerOptions()
+        // TODO: attach generated metadata here
+        opts
+
 type StjBackend() =
     interface ISerdeBackend with
         member _.Serialize(value, _options) =
-            JsonSerializer.Serialize(value, JsonSerializerOptions())
+            JsonSerializer.Serialize(value, StjOptionsCache.defaultJsonOptions)
 
         member _.Deserialize(json, _options) =
-            JsonSerializer.Deserialize<'T>(json, JsonSerializerOptions())
+            JsonSerializer.Deserialize<'T>(json, StjOptionsCache.defaultJsonOptions)
