19dec3f2ded50475b70ac7763e8867d3c992e876
Jordan Marr <JordanMarr@users.noreply.github.com>
2026-03-22 18:42:29 -0400

    Added authorization policy.

--- src/Serde.FS.Json.SampleRpc.Server/Program.fs
+++ src/Serde.FS.Json.SampleRpc.Server/Program.fs
@@ -1,19 +1,61 @@
 module SampleRpc.Server.Program
 
 open Microsoft.AspNetCore.Builder
-open Serde.FS.Json.AspNet
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Authentication
 open SampleRpc.Shared
 open SampleRpc.Server.OrderApi
+open Serde.FS.Json.AspNet
+
+module Handlers =
+    open System.Security.Claims
+
+    type ApiKeyAuthHandler(options, logger, encoder) =
+        inherit AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder)
+
+        override this.HandleAuthenticateAsync() =
+            let request = this.Request
+            task {
+                match request.Headers.TryGetValue("X-Api-Key") with
+                | true, v when v.ToString() = "ABC" ->
+                    let claims = [| Claim(ClaimTypes.Name, "ApiKeyUser") |]
+                    let identity = ClaimsIdentity(claims, "ApiKey")
+                    let principal = ClaimsPrincipal(identity)
+                    let ticket = AuthenticationTicket(principal, "ApiKey")
+                    return AuthenticateResult.Success(ticket)
+                | _ ->
+                    return AuthenticateResult.Fail("Missing or invalid X-Api-Key header")
+            }
+
 
 [<Serde.FS.EntryPoint>]
 let main (argv: string array) =
     let builder = WebApplication.CreateBuilder(argv)
+
+    builder.Services
+        .AddAuthentication("ApiKey")
+        .AddScheme<AuthenticationSchemeOptions, Handlers.ApiKeyAuthHandler>("ApiKey", ignore)
+    |> ignore
+
+    builder.Services.AddAuthorization(fun options ->
+        options.AddPolicy("ApiKeyPolicy", fun policy ->
+            policy.RequireAuthenticatedUser() |> ignore
+        )
+    ) |> ignore
+
     let app = builder.Build()
+    app.UseAuthentication() |> ignore
+    app.UseAuthorization() |> ignore
 
-    app.MapRpcApi<IOrderApi>(OrderApi()) |> ignore
-    app.MapGet("/", System.Func<string>(fun () -> "SampleRpc.Server — RPC endpoints at /rpc/IOrderApi/{method}")) |> ignore
+    let rpc = app.MapRpcApi<IOrderApi>(OrderApi())
 
-    printfn "Starting RPC server..."
+    rpc.GetRoute(nameof Unchecked.defaultof<IOrderApi>.PlaceOrder)
+        .RequireAuthorization("ApiKeyPolicy") |> ignore
+
+    app.MapGet("/", System.Func<string>(fun () -> "Serde.FS.Json SampleApp — RPC endpoints at /rpc/{method}")) |> ignore
+
+    printfn "Starting web server..."
     printfn "Use RpcApi.http to test the API"
     app.Run()
+
     0
